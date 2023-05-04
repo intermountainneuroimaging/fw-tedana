13ce46bac5ae928f2ecdbac9b1f92461a5f69123
amyhegarty <amy.hegarty@colorado.edu>
2023-05-04 12:16:49 -0600

    bug fix for multi acq sessions

--- fw_gear_tedana/main.py
+++ fw_gear_tedana/main.py
@@ -67,7 +67,7 @@ def prepare(
 
 
 def run(gear_options: dict, app_options: dict) -> int:
-    """Run tedana using fmriprep or manual inputs.
+    """launch workflow for fmriprep or manual inputs.
 
     Arguments:
         gear_options: dict with gear-specific options
@@ -81,68 +81,102 @@ def run(gear_options: dict, app_options: dict) -> int:
 
     # Loop through tasks if fmriprep is passed as input
     if app_options["inputtype"] == "fmriprep":
-        path = op.join(gear_options["fmriprep-dir"], "sub-"+app_options["sid"], "ses-"+app_options["sesid"])
-        tasks = fmriprep_get_tasks(path)
+        run_error = run_fmriprep_pipe(gear_options, app_options)
 
-        # run tedana workflow for each task
-        for task in tasks:
-            echo_files = fmriprep_get_echos(path, task)
+    else:
+        run_error = run_manual_pipe(gear_options, app_options)
 
-            if '' in echo_files:
-                continue
+    return run_error
 
-            # pull echo time from .json
-            echo_times = [] ; arg_options = dict()
-            for e in echo_files:
-                with open(e.replace("nii.gz","json")) as f:
-                    dat = json.load(f)
-                    echo_times.append(dat["EchoTime"]*1000)
 
-            log.info("Using multiecho files: %s", "\n".join(echo_files))
-            log.info("Using echo times (ms): %s", str(echo_times))
+def run_fmriprep_pipe(gear_options: dict, app_options: dict) -> int:
+    """Run tedana using fmriprep inputs.
 
-            arg_options["d"] = " ".join(echo_files)
-            arg_options["e"] = " ".join([str(i) for i in echo_times])
+    Arguments:
+        gear_options: dict with gear-specific options
+        app_options: dict with options for the BIDS-App
 
-            index = [idx for idx, s in enumerate(echo_files[0].split("_")) if 'echo' in s][0]
-            arg_options["prefix"] = "_".join(echo_files[0].split("/")[-1].split("_")[0:index])
+    Returns:
+        run_error: any error encountered running the app. (0: no error)
+    """
 
-            if app_options["explicit-mask"]:
-                f = searchfiles(app_options["prefix"]+"_desc-brain_mask.nii.gz")
-                arg_options["mask"] = f[0]
-                log.info("Explicit mask requested using file: %s", f[0])
+    path = op.join(gear_options["fmriprep-dir"], "sub-" + app_options["sid"], "ses-" + app_options["sesid"])
+    tasks = fmriprep_get_tasks(path)
 
-            if app_options["output-layout"] == "bids":
-                output_analysis_id_dir = op.join(gear_options["work-dir"],gear_options["destination-id"],"fmriprep","derivatives","tedana", "sub-" + app_options["sid"], "ses-" + app_options["sesid"],"func")
-                os.makedirs(output_analysis_id_dir, exist_ok=True)
-                arg_options["out-dir"] = output_analysis_id_dir
-            else:
-                output_analysis_id_dir = op.join(gear_options["work-dir"], gear_options["destination-id"], "fmriprep", "sub-" + app_options["sid"], "ses-" + app_options["sesid"],"func")
-                os.makedirs(output_analysis_id_dir, exist_ok=True)
-                arg_options["out-dir"] = output_analysis_id_dir
+    # run tedana workflow for each task
+    for task in tasks:
+        echo_files = fmriprep_get_echos(path, task)
 
-            arg_options["command-line-args"] = app_options["command-line-args"]
+        if '' in echo_files:
+            continue
 
-            # generate command
-            command = generate_command(gear_options, arg_options)
+        # pull echo time from .json
+        echo_times = [];
+        arg_options = dict()
+        for e in echo_files:
+            with open(e.replace("nii.gz", "json")) as f:
+                dat = json.load(f)
+                echo_times.append(dat["EchoTime"] * 1000)
 
-    if error_handler.fired:
-        log.critical('Failure: exiting with code 1 due to logged errors')
-        run_error = 1
-        return run_error
+        log.info("Using multiecho files: %s", "\n".join(echo_files))
+        log.info("Using echo times (ms): %s", str(echo_times))
 
-    # This is what it is all about
-    stdout, stderr, run_error = exec_command(
-        command,
-        dry_run=gear_options["dry-run"],
-        shell=True,
-        cont_output=True,
-        cwd=gear_options["work-dir"]
-    )
+        arg_options["d"] = " ".join(echo_files)
+        arg_options["e"] = " ".join([str(i) for i in echo_times])
+
+        index = [idx for idx, s in enumerate(echo_files[0].split("/")[-1].split("_")) if 'echo' in s][0]
+        arg_options["prefix"] = "_".join(echo_files[0].split("/")[-1].split("_")[0:index])
+
+        if app_options["explicit-mask"]:
+            f = searchfiles(app_options["prefix"] + "_desc-brain_mask.nii.gz")
+            arg_options["mask"] = f[0]
+            log.info("Explicit mask requested using file: %s", f[0])
+
+        if app_options["output-layout"] == "bids":
+            output_analysis_id_dir = op.join(gear_options["work-dir"], gear_options["destination-id"], "fmriprep",
+                                             "derivatives", "tedana", "sub-" + app_options["sid"],
+                                             "ses-" + app_options["sesid"], "func")
+            os.makedirs(output_analysis_id_dir, exist_ok=True)
+            arg_options["out-dir"] = output_analysis_id_dir
+        else:
+            output_analysis_id_dir = op.join(gear_options["work-dir"], gear_options["destination-id"], "fmriprep",
+                                             "sub-" + app_options["sid"], "ses-" + app_options["sesid"], "func")
+            os.makedirs(output_analysis_id_dir, exist_ok=True)
+            arg_options["out-dir"] = output_analysis_id_dir
+
+        arg_options["command-line-args"] = app_options["command-line-args"]
+
+        # generate command
+        command = generate_command(gear_options, arg_options)
+
+        if error_handler.fired:
+            log.critical('Failure: exiting with code 1 due to logged errors')
+            run_error = 1
+            return run_error
+
+        # This is what it is all about
+        stdout, stderr, run_error = exec_command(
+            command,
+            dry_run=gear_options["dry-run"],
+            shell=True,
+            cont_output=True,
+            cwd=gear_options["work-dir"]
+        )
+
+        # if gear completed without error, move report to also contain acq prefix
+        if run_error == 0:
+            files = searchfiles(os.path.join(arg_options["out-dir"], "tedana*"), dryrun=False)
+            for f in files:
+                f = Path(f)
+                if arg_options["prefix"] in f.name:
+                    continue
+                os.rename(str(f), str(f).replace("tedana", arg_options["prefix"]))
+
+            # Make archives for result *.html files for easy display on platform
+            zip_htmls(gear_options["output-dir"], gear_options["destination-id"], output_analysis_id_dir)
 
     if not gear_options["dry-run"]:
-
-        #zip outputs
+        # zip outputs
         zipname = "tedana_" + app_options["sesid"] + "_" + gear_options["destination-id"]
         cmd = "zip -r " + os.path.join(gear_options["output-dir"], zipname) + ".zip " + gear_options["destination-id"]
         execute_shell(cmd, dryrun=gear_options["dry-run"], cwd=gear_options["work-dir"])
@@ -153,14 +187,33 @@ def run(gear_options: dict, app_options: dict) -> int:
     return run_error
 
 
+def run_manual_pipe(gear_options: dict, app_options: dict) -> int:
+    """Run tedana using manual inputs.
+
+    Arguments:
+        gear_options: dict with gear-specific options
+        app_options: dict with options for the BIDS-App
+
+    Returns:
+        run_error: any error encountered running the app. (0: no error)
+    """
+
+    # TODO write this method!
+
+    log.info("manual input run not currently supported")
+    pass
+
+
 def fmriprep_get_tasks(path):
 
     # search for all func "bold.nii.gz" func files to choose from..
     results = []
-    files = searchfiles(os.path.join(path,"func","*bold.nii.gz"))
+    files = searchfiles(os.path.join(path,"func","*echo*bold.nii.gz"))
     for f in files:
-        name_split = f.split("_")
-        res = [i for i in name_split if "task" in i][0]
+        index1 = [idx for idx, s in enumerate(f.split("/")[-1].split("_")) if 'task' in s][0]
+        index2 = [idx for idx, s in enumerate(f.split("/")[-1].split("_")) if 'echo' in s][0]
+        res = "_".join(f.split("/")[-1].split("_")[index1:index2])
+
         results.append(res)
 
     return list(set(results))
